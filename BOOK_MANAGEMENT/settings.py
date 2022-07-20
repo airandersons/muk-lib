50e47c129a7030b64bf0de6fc568a9dccb9b4c64
MWANJE ISMA SSERUNKUMA <mwanjeisma5@gmail.com>
2022-07-20 08:13:51 +0300

    turned debug to off

--- BOOK_MANAGEMENT/settings.py
+++ BOOK_MANAGEMENT/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#d6mwowtb6bcy^$ayhc^+d(nx+^@nc59vg$j&6i#(9gjqlc4&7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
